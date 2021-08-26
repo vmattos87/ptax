94d5b7c27742335ad5e3ed97c960ee24ee2fa695
Vinícius Mattos <84902205+vmattos87@users.noreply.github.com>
2021-08-26 17:38:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 from foo import query, save, usr_input
 
-
 def get_USD(url, params):
     rows = query.fetch(url, params)
     if query.get_values_USD(rows):
         save.save_CSV_USD(rows)
-
 
 def get_EUR(url, params):
     rows = query.fetch(url, params)    
     if query.get_values_EUR(rows):
         save.save_CSV_EUR(rows)
 
-
 url = 'https://ptax.bcb.gov.br/ptax_internet/consultaBoletim.do'
 COD_USD = '61'
 COD_EUR = '222'
-
 
 params = {'method': 'consultarBoletim',
           'RadOpcao': '1',
