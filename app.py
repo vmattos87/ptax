ff97ad94a293c9afcf4943cf5a1426641d8bb618
Vinícius Mattos <84902205+vmattos87@users.noreply.github.com>
2021-08-26 20:41:38 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from foo import query, save, usr_input
 
+
 def get_USD(url, params):
     rows = query.fetch(url, params)
     if query.get_values_USD(rows):
         save.save_CSV_USD(rows)
 
+
 def get_EUR(url, params):
-    rows = query.fetch(url, params)    
+    rows = query.fetch(url, params)
     if query.get_values_EUR(rows):
         save.save_CSV_EUR(rows)
+
 
 url = 'https://ptax.bcb.gov.br/ptax_internet/consultaBoletim.do'
 COD_USD = '61'
@@ -20,13 +23,19 @@ params = {'method': 'consultarBoletim',
           'DATAINI': '',
           'DATAFIM': ''}
 
-print('Consulta ao PTAX do Dólar e Euro\n')
-print('Após a consulta as cotações do Dólar e Euro serão salvas em arquivos .CSV\n\n')
 
-params['DATAINI'], params['DATAFIM'] = usr_input.get_date()
+def main():
+    print('Consulta ao PTAX do Dólar e Euro\n')
+    print('Após a consulta as cotações do Dólar e Euro serão salvas em arquivos .CSV\n\n')
 
-params['ChkMoeda'] = COD_USD
-get_USD(url, params)
+    params['DATAINI'], params['DATAFIM'] = usr_input.get_date()
 
-params['ChkMoeda'] = COD_EUR
-get_EUR(url, params)
+    params['ChkMoeda'] = COD_USD
+    get_USD(url, params)
+
+    params['ChkMoeda'] = COD_EUR
+    get_EUR(url, params)
+
+
+if __name__ == '__main__':
+    main()
